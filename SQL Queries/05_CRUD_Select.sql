2ff3e1c73ef9b93379df61fa9879fbad7c5b2efc
Hager Sayed <hagersayed0210@gmail.com>
2026-01-22 17:01:55 +0100

    Add CRUD Select

---
+++ SQL Queries/05_CRUD_Select.sql
@@ -0,0 +1,17 @@
+USE MiniEcommerceDB;
+GO
+
+-- Visa alla kunder
+SELECT * FROM Customers;
+
+-- Visa alla produkter
+SELECT * FROM Products;
+
+-- Visa alla ordrar
+SELECT * FROM Orders;
+
+-- Visa alla betalningar
+SELECT * FROM Payments;
+
+-- Visa orderrader
+SELECT * FROM OrderItems;
